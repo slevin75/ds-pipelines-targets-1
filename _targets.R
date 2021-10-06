0fb3659f2200e08ae26b2825d4e2ee7538a109d7
Levin <slevin@usgs.gov>
2021-10-06 16:25:58 -0500

    changed targets.R

--- _targets.R
+++ _targets.R
@@ -1,18 +1,25 @@
 library(targets)
-source("code.R")
+library(dplyr)
+library(readr)
+library(stringr)
+
+source("./1_fetch/src/fetch_functions.R")
+source("./2_process/src/process_functions.R")
+source("./3_visualize/src/plot_functions.R")
 tar_option_set(packages = c("tidyverse", "sbtools", "whisker"))
 
 list(
   # Get the data from ScienceBase
   tar_target(
     model_RMSEs_csv,
-    download_data(out_filepath = "1_fetch/out/model_RMSEs.csv"),
+    download_data(outdir="1_fetch/out",fname= "model_RMSE.csv",sb_id='5d925066e4b0c4f70d0d0599',item_name='me_RMSE.csv'),
     format = "file"
   ), 
   # Prepare the data for plotting
   tar_target(
     eval_data,
-    process_data(in_filepath = model_RMSEs_csv),
+    process_data(data=eval_data,outdir='2_process/out', fname='model_summary_results.csv', 
+                 colors=c('#1b9e77','#d95f02','#7570b3'),shapes=c(21,22,23)),
   ),
   # Create a plot
   tar_target(
