53243323e17a3b1abf5c146ef29e66a516e32c37
Levin <slevin@usgs.gov>
2021-10-06 13:37:32 -0500

    created function to make mod. diagnostics file and filter function

--- 1_fetch/src/fetch_functions.R
+++ 1_fetch/src/fetch_functions.R
@@ -3,7 +3,7 @@
 #function to download raw data from Science Base
 
 download_data<-function(outdir,fname,sb_id,item_name){
-  data_file <- file.path( outdir, 'model_RMSEs.csv')
+  data_file <- file.path( outdir, fname)
   item_file_download(sb_id, names = item_name, destinations = data_file, overwrite_file = TRUE)
   data<-read.csv(data_file)
 
--- 3_visualize/src/plot_functions.R
+++ 3_visualize/src/plot_functions.R
@@ -47,25 +47,36 @@ plot_data<-function(eval_data, outdir, fname_plot,fname_txt, width, height,res,u
   text(2.3, 1.1, 'Process-Based', pos = 4, cex = 1.1)
   
   dev.off()
-  
+}
+
+
+
+
+
+filter_function<-function(data,model_type,exper_id,roundto){
+  filter(data, model_type == model_type, exper_id ==exper_id) %>% pull(rmse) %>% mean %>% round(roundto)
+}
+
+
+
+make_diagnostics_file<-function(data,outdir,fname){
   # Save the model diagnostics
-  render_data <- list(pgdl_980mean = filter(eval_data, model_type == 'pgdl', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
-                      dl_980mean = filter(eval_data, model_type == 'dl', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
-                      pb_980mean = filter(eval_data, model_type == 'pb', exper_id == "similar_980") %>% pull(rmse) %>% mean %>% round(2),
-                      dl_500mean = filter(eval_data, model_type == 'dl', exper_id == "similar_500") %>% pull(rmse) %>% mean %>% round(2),
-                      pb_500mean = filter(eval_data, model_type == 'pb', exper_id == "similar_500") %>% pull(rmse) %>% mean %>% round(2),
-                      dl_100mean = filter(eval_data, model_type == 'dl', exper_id == "similar_100") %>% pull(rmse) %>% mean %>% round(2),
-                      pb_100mean = filter(eval_data, model_type == 'pb', exper_id == "similar_100") %>% pull(rmse) %>% mean %>% round(2),
-                      pgdl_2mean = filter(eval_data, model_type == 'pgdl', exper_id == "similar_2") %>% pull(rmse) %>% mean %>% round(2),
-                      pb_2mean = filter(eval_data, model_type == 'pb', exper_id == "similar_2") %>% pull(rmse) %>% mean %>% round(2))
+  render_data <- list(pgdl_980mean = filter_function(eval_data,'pgdl', "similar_980",2),
+                      dl_980mean = filter_function(eval_data, 'dl', "similar_980",2),
+                      pb_980mean = filter_function(eval_data, 'pb', "similar_980",2),
+                      dl_500mean = filter_function(eval_data, 'dl', "similar_500",2),
+                      pb_500mean = filter_function(eval_data, 'pb', "similar_500",2),
+                      dl_100mean = filter_function(eval_data, 'dl',"similar_100",2),
+                      pb_100mean = filter_function(eval_data, 'pb', "similar_100",2) ,
+                      pgdl_2mean = filter_function(eval_data, 'pgdl',"similar_2",2),
+                      pb_2mean = filter_function(eval_data, 'pb', "similar_2",2) )
   
   template_1 <- 'resulted in mean RMSEs (means calculated as average of RMSEs from the five dataset iterations) of {{pgdl_980mean}}, {{dl_980mean}}, and {{pb_980mean}}°C for the PGDL, DL, and PB models, respectively.
   The relative performance of DL vs PB depended on the amount of training data. The accuracy of Lake Mendota temperature predictions from the DL was better than PB when trained on 500 profiles 
   ({{dl_500mean}} and {{pb_500mean}}°C, respectively) or more, but worse than PB when training was reduced to 100 profiles ({{dl_100mean}} and {{pb_100mean}}°C respectively) or fewer.
   The PGDL prediction accuracy was more robust compared to PB when only two profiles were provided for training ({{pgdl_2mean}} and {{pb_2mean}}°C, respectively). '
   
-  whisker.render(template_1 %>% str_remove_all('\n') %>% str_replace_all('  ', ' '), render_data ) %>% cat(file = file.path(outdir,fname_txt))
+  whisker.render(template_1 %>% str_remove_all('\n') %>% str_replace_all('  ', ' '), render_data ) %>% cat(file = file.path(outdir,fname))
+  
   
 }
-
-
